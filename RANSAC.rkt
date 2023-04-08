a608a631a0df31ef157a64818934d022acad4e96
pranav-kural <pranavkural042@gmail.com>
2023-04-08 15:47:37 -0400

    initial project setup

---
+++ RANSAC.rkt
@@ -0,0 +1,6 @@
+#lang scheme
+
+
+
+; function to pick 3 random points from list of points
+(list-ref Ps (random (length Ps)))
---
+++ utilities.rkt
@@ -0,0 +1,40 @@
+#lang scheme
+
+; Utility functions
+
+; DEBUG Mode
+(define DEBUG #f)
+
+; function to update debug mode
+(define (set-debug debugMode)
+  (set! DEBUG debugMode)
+  (printlist (list "DEBUG mode : " DEBUG)))
+
+; function to print message and then a newline
+(define println
+  (lambda (x)
+    (display x)
+    (newline)))
+
+; function to display elements of given list where first element is title and tail contains list to be printed
+(define (printlist lst)
+  (display (car lst))
+  (for-each display (cdr lst))
+  (newline))
+    
+; print when DEBUG is ON
+(define (printd x)
+  (if (equal? DEBUG #t)
+      (println x)
+      (display "")))
+    
+
+; if debug mode is ON, given list of elements, print each element on a new line
+(define (printdl x)
+  (if (eqv? DEBUG #t)
+      (if (null? (cdr x)) ; if at end of list
+          (println (car x)) ; print last element
+          (and
+           (println (car x))
+           (printdl (cdr x)))) ; recursively call for printing next element
+       (display "")))
