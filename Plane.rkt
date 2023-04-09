ce2224577a247f9b2647bb18732413d5b00d8274
pranav-kural <pranavkural042@gmail.com>
2023-04-09 17:57:39 -0400

    functions to get distance and plane support

--- Plane.rkt
+++ Plane.rkt
@@ -1,7 +1,7 @@
 #lang scheme
 
 ; functions to work with a 3D Plane
-(provide diff plane)
+(provide diff plane getDistanceFromPlane getPlaneSupport)
 
 ; function to get equation of a plane in 3D given list of 3 points
 (define (plane p1 p2 p3)
@@ -22,3 +22,29 @@
 (define (diff p1 p2)
   ; (p2.x - p1.x, p2.y - p1.y, p2.z - p1.z)
   (list (- (first p2) (first p1)) (- (second p2) (second p1)) (- (third p2) (third p1))))
+
+; function to calculate distance from point to plane
+(define (getDistanceFromPlane p1 plane)
+  (let (
+        (a (first plane))
+        (b (second plane))
+        (c (third plane))
+        (d (fourth plane)))
+  (/ (abs (+ (* a (first p1)) (* b (second p1)) (* c (third p1)) d)) (sqrt(+ (* a a) (* b b) (* c c))))))
+
+; function to calculate the support of a plane
+; Pseudo-code
+; Given a 3D plane equation and a list of all 3D points of a point cloud
+;  - initialize variable to hold support of the plane with initial value zero
+;  - Loop: through each 3D Point
+;    - If: distance of point to given plane <= eps
+;      - increment the support of plane
+;  - Return: a pair containing plane and number of supporting points
+(define (getPlaneSupport plane points eps)
+  (define support 0) ; initial value
+  (for-each
+   (lambda (point)
+     (if (<= (getDistanceFromPlane point plane) eps)
+         (set! support (+ 1 support))
+         #t)) points)
+  (cons plane support))
--- Test.rkt
+++ Test.rkt
@@ -5,25 +5,29 @@
 (require "RANSACFiles.rkt") ; function to read and write to file
 (require "utilities.rkt") ; generic utilities
 
-(println "Running Test")
+; Test functions to test important core methods
+; Most of the expected values against which actual function outputs are compared with, have been generated from
+; a verified Java based RANSAC implementation which is known to produce correct values
 
-; set debug mode
-(set-debug #t)
-
-; sample filename with path
+; store test point cloud filename and path globally - used in some tests (like test-read, test-support, etc.)
 (define TEST_FILE "./data/input/Point_Cloud_1_No_Road_Reduced.xyz")
 
-; read file and load the list of 3D points
-(define Ps (readXYZ TEST_FILE))
+(define (test-read)
+  (println "-------- Tesing Reading Point Cloud -------- ")
+  (println "Reading Sample Point Cloud file and displaying randomly selected 3 points")
+ 
+  ; read file and load the list of 3D points
+  (define Ps (readXYZ TEST_FILE))
 
-; function to pick 3 random points from list of points
-(define Point1 (list-ref Ps (random (length Ps))))
-(define Point2 (list-ref Ps (random (length Ps))))
-(define Point3 (list-ref Ps (random (length Ps))))
+  ; function to pick 3 random points from list of points
+  (define Point1 (list-ref Ps (random (length Ps))))
+  (define Point2 (list-ref Ps (random (length Ps))))
+  (define Point3 (list-ref Ps (random (length Ps))))
 
-; function to generate plane equation
-(printlistn "Selected random points: " (list Point1 Point2 Point3))
-;(println Point1)
+  ; function to generate plane equation
+  (printlistn "Selected random points: " (list Point1 Point2 Point3))
+  (println "-------- Test Finished -------- "))
+  
 
 ; Testing diff
 (define (test-diff)
@@ -36,7 +40,7 @@
                "#: Test Passed"
                "#: Test Failed"))
        ))
-(test-diff)
+
 
 ;Testing Plane equation calculation
 (define (test-plane)
@@ -56,4 +60,48 @@
                "#: Test Failed"))
        )
     )
-(test-plane)
+
+
+; Test: Getting distance of point to plane
+(define (test-distance)
+  (let* (
+         (p1 '(12.1422584 15.95857346 5.677742716))
+         (plane '(-19.24478941973517 -22.416438566567404 84.2872272864299 74.52946568859088))
+         (expected 0.4290306042682359)
+         (actual (getDistanceFromPlane p1 plane)))
+  (println "-------- Testing Distance from Plane --------")
+  (newline)
+  (printlistn "Test parameters" (list (list "Test point: " p1) (list "plane: " plane) (list " Expected distance: " expected)))
+  (printlist (list "Actual distance: " actual))
+  (println (if (equal? actual expected)
+               "#: Test Passed"
+               "#: Test Failed"))
+       ))
+
+(define (test-support)
+  (let* (
+         (points (readXYZ TEST_FILE))
+         (plane '(-19.24478941973517 -22.416438566567404 84.2872272864299 74.52946568859088))
+         (eps 0.5)
+         (expected 1532)
+         (actual (cdr (getPlaneSupport plane points eps))))
+  (println "-------- Testing Plane Support --------")
+  (newline)
+  (printlistn "Test parameters" (list (list "Test plane: " plane) (list "eps: " eps) (list "Size of point cloud: " (length points)) (list "Expected support: " expected)))
+  (printlist (list "Actual support: " actual))
+  (println (if (equal? actual expected)
+               "#: Test Passed"
+               "#: Test Failed"))
+       ))
+
+; Main test function
+; Can comment out functions which you don't want to run the test for
+(define (run-tests)
+  ;(test-read)
+  ;(test-diff)
+  ;(test-plane)
+  ;(test-distance)
+  (test-support)
+  )
+
+(run-tests)
